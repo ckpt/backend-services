30b435e6d21e7f3b5e0d9c6906031c3bb2489b28
Morten Knutsen <morten.knutsen@uninett.no>
2015-06-04 11:41:35 +0200

    Add simple dummy member post code.

--- dummydata.go
+++ dummydata.go
@@ -6,6 +6,30 @@ import (
 	"github.com/m4rw3r/uuid"
 )
 
+var dummyUUIDs = createUUIDs(5)
+var dummyMembers = []Member{
+	Member{
+		UUID: dummyUUIDs[0],
+		Profile: MemberProfile{
+			Birthday: time.Date(1979, time.April, 14, 0, 0, 0, 0, time.Local),
+			Name: "Morten Knutsen",
+			Email: "knumor@gmail.com",
+		},
+		Nick: "Panzer",
+		Active: true,
+	},
+	Member{
+		UUID: dummyUUIDs[1],
+		Profile: MemberProfile{
+			Birthday: time.Date(1979, time.October, 20, 0, 0, 0, 0, time.Local),
+			Name: "Bjørn Helge Kopperud",
+			Email: "bjorn@kjekkegutter.no",
+		},
+		Nick: "Bjøro",
+		Active: true,
+	},
+}
+
 func createUUIDs(number int) []uuid.UUID {
 	var uuids []uuid.UUID
 	for number > 0 {
@@ -29,27 +53,6 @@ func getMember(memberID uuid.UUID) (Member, error) {
 	return Member{}, errors.New("Not found")
 }
 
-
-var dummyUUIDs = createUUIDs(5)
-var dummyMembers = [...]Member{
-	Member{
-		UUID: dummyUUIDs[0],
-		Profile: MemberProfile{
-			Birthday: time.Date(1979, time.April, 14, 0, 0, 0, 0, time.Local),
-			Name: "Morten Knutsen",
-			Email: "knumor@gmail.com",
-		},
-		Nick: "Panzer",
-		Active: true,
-	},
-	Member{
-		UUID: dummyUUIDs[1],
-		Profile: MemberProfile{
-			Birthday: time.Date(1979, time.October, 20, 0, 0, 0, 0, time.Local),
-			Name: "Bjørn Helge Kopperud",
-			Email: "bjorn@kjekkegutter.no",
-		},
-		Nick: "Bjøro",
-		Active: true,
-	},
+func addMember(newMember Member) {
+	dummyMembers = append(dummyMembers[:], newMember)
 }
--- main.go
+++ main.go
@@ -27,10 +27,24 @@ func member(c web.C, w http.ResponseWriter, r *http.Request) {
 	encoder.Encode(member)
 }
 
+func newMember(c web.C, w http.ResponseWriter, r *http.Request) {
+    var nMember Member
+	decoder := json.NewDecoder(r.Body)
+    err := decoder.Decode(&nMember)
+    if err != nil {
+        http.Error(w, err.Error(), 500)
+    }
+	uuid, _ := uuid.V4()
+	nMember.UUID = uuid
+	addMember(nMember)
+	w.Header().Set("Location", "/member/" + uuid.String())
+    w.WriteHeader(201)
+}
 
 func main() {
 	fmt.Printf("%+v\n", getMembers())
 	goji.Get("/members", members)
+	goji.Post("/members", newMember)
 	goji.Get("/member/:uuid", member)
 	goji.Serve()
 }
